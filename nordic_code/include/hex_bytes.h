6367fe35b837b56cb54a256c73eae110c92445b2
makrron <mvazquezarenal@uma.es>
2024-05-06 19:08:59 +0200

    Add hex_bytes and crypto modules, update main.c

---
+++ nordic_code/include/hex_bytes.h
@@ -0,0 +1,10 @@
+#ifndef HEX_TO_BYTES_H
+#define HEX_TO_BYTES_H
+
+#include <stddef.h> // para size_t
+
+// Declara la función hex_to_bytes para que pueda ser utilizada por otros archivos
+int hex_to_bytes(const char *hex, unsigned char *bytes, size_t bytes_len);
+char* bytes_to_hex(const unsigned char* bytes, size_t len);
+
+#endif // HEX_TO_BYTES_H
--- nordic_code/src/main.c
+++ nordic_code/src/main.c
@@ -1,26 +1,41 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "privateKey.h"
+#include "publicKey.h"
+#include "hex_bytes.h"
 
 int main() {
-	char* private_key = generate_private_key();
-	if (!private_key) {
-		perror("Failed to generate private key");
-		return 1;
-	}
+    char* private_key = generate_private_key();
+    if (!private_key) {
+        printk("Failed to generate private key\n");
+        return 1;
+    }
 
-	printk("Clave Privada: %s\n", private_key);
+    printk("Clave Privada: %s\n", private_key);
 
-	char* wif = convert_private_key_to_wif(private_key, 1); // 1 para testnet, 0 para mainnet
-	if (!wif) {
-		perror("Failed to convert private key to WIF");
-		return 1;
-	}
+    char* wif = convert_private_key_to_wif(private_key, 1);
+    if (!wif) {
+        printk("Failed to convert private key to WIF\n");
+        free(private_key);
+        return 1;
+    }
 
-	printk("Clave Privada WIF: %s\n", wif);
+    printk("Clave Privada WIF: %s\n", wif);
 
-	free(private_key);
-	free(wif);
+    unsigned char public_key[33]; // Tamaño correcto para clave pública comprimida
+    size_t public_key_len = sizeof(public_key);
+    if (generate_public_key_from_hex(private_key, public_key, &public_key_len)) {
+        char* public_key_hex = bytes_to_hex(public_key, public_key_len);
+        printk("Hex Public Key: %s\n", public_key_hex);
+        free(public_key_hex);
+    } else {
+        printk("Failed to generate public key\n");
+    }
 
-	return 0;
+    free(private_key);
+    free(wif);
+
+    return 0;
 }
